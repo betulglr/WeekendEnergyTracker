ae70de56ab4a02ae848e1c055cc7acc3546654d9
Betül GÜLER <betul.guler@micromarin.com>
2025-06-15 17:57:28 +0300

    Add ThemeService for dynamic theme management
    Introduce ThemeService to handle light/dark theme switching. Integrate RadzenTheme in App.razor and MainLayout.razor. Update Program.cs to register ThemeService and Radzen components. Refine layout in MainLayout.razor and adjust formatting in NavMenu.razor. Update Home.razor to include a new main UI container.

--- WeekendEnergyTracker/Program.cs
+++ WeekendEnergyTracker/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
+using Radzen.Blazor;
 using WeekendEnergyTracker;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +9,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddRadzenComponents();
+builder.Services.AddScoped<ThemeService>();
+
 
 await builder.Build().RunAsync();

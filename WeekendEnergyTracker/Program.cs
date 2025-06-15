ba9da019d5fc50e1b852fdc41421875e624d24f8
darklexa <clexapage@gmail.com>
2025-06-15 21:24:29 +0300

    Revert "Add ThemeService for dynamic theme management"
    This reverts commit ae70de56ab4a02ae848e1c055cc7acc3546654d9.

--- WeekendEnergyTracker/Program.cs
+++ WeekendEnergyTracker/Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Radzen;
-using Radzen.Blazor;
 using WeekendEnergyTracker;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,8 +7,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddRadzenComponents();
-builder.Services.AddScoped<ThemeService>();
-
 
 await builder.Build().RunAsync();
